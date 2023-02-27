fe193364e4eeb8030f7bd126559beb76f00c02cd
João Victor Rodrigues <joaorodriguesvictor@outlook.com>
2023-02-26 22:21:29 -0300

    refatorando imc

--- Logica_de_programacao/imc.dart
+++ Logica_de_programacao/imc.dart
@@ -1,54 +1,54 @@
 import 'dart:io';
 
 void main() {
-  CalculoIMC calc = CalculoIMC();
-  calc.calcIMC();
+  final String name = pegarNome();
+
+  double peso = pegarPeso();
+  double altura = pegarAltura();
+
+  double resultado = calcularImc(peso: peso, altura: altura);
+
+  print('\n\n');
+  print('Nome: $name');
+  if (resultado > 25) {
+    print('você está gordo');
+  } else if (resultado < 18) {
+    print('você está magro');
+  } else {
+    print('você está com peso normal');
+  }
 }
 
-class CalculoIMC {
-  void calcIMC() {
-    String name = qualNome();
-    double peso = qualPeso();
-    double altura = qualAltura();
-    double resultado = peso / (altura * altura);
-    String resultText = resultado.toStringAsFixed(2);
+double calcularImc({required double peso, required double altura}) {
+  return peso / (altura * altura);
+}
 
-    if (resultado > 25) {
-      print("$name , seu IMC é $resultText, portanto, você está gordo !");
-    } else if (resultado < 18) {
-      print("$name , seu IMC é $resultText, portanto, você está magro !");
-    } else {
-      print("$name , seu IMC é $resultText, portanto, você está bem !");
-    }
+String pegarNome() {
+  print('Digite seu nome');
+  final String? name = stdin.readLineSync();
+  if (name == null) {
+    return 'Anônimo';
+  } else {
+    return name;
   }
+}
 
-  String qualNome() {
-    print("Qual é o seu nome ?");
-    String? name = stdin.readLineSync();
-    if (name == null) {
-      return "Nome não informado";
-    } else {
-      return name;
-    }
+double pegarPeso() {
+  print('Digite seu peso');
+  final String? pesoString = stdin.readLineSync();
+  if (pesoString == null) {
+    return 0.0;
+  } else {
+    return double.parse(pesoString);
   }
+}
 
-  double qualPeso() {
-    print("Qual é o seu peso ?");
-    String? peso = stdin.readLineSync();
-    if (peso == null) {
-      return 0.0;
-    } else {
-      return double.parse(peso);
-    }
-  }
-
-  double qualAltura() {
-    print("Qual é a sua altura ?");
-    String? altura = stdin.readLineSync();
-    if (altura == null) {
-      return 0.0;
-    } else {
-      return double.parse(altura);
-    }
+double pegarAltura() {
+  print('Digite sua altura');
+  final String? alturaString = stdin.readLineSync();
+  if (alturaString == null) {
+    return 0.0;
+  } else {
+    return double.parse(alturaString);
   }
 }
